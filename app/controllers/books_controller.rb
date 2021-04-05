cf5575def8701b6b3d5311077701df3ed2ff9d73
Koala777100 <s_n_s_h@outlook.jp>
2021-04-05 15:34:48 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,30 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.id = current_user.id
+    @book.save
+    redirect_to book_path(current_user.id)
   end
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :profile_image, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :books
 
   get 'home/about' => 'homes#about'
-
+#質問
+  post 'books' => 'books#create'
+  post 'user/show' => 'books#create'
 end
