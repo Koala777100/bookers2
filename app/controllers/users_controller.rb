6a87e477006e59017aec0ec857c625586c7d253e
Koala777100 <s_n_s_h@outlook.jp>
2021-04-05 11:34:53 +0000

    Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
   def new
+
   end
 
   def create
+    @user = User.new(user_params)
+    @user.id = current_user.id
+    @user.save
+    redirect_to user_path
   end
 
   def index
@@ -12,8 +17,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def destroy
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image_id, :introduction)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :users, only: [:new, :create, :index, :show, :edit, :destroy]
-  get 'books/index' => 'books#index'
-  get 'books/show' => 'books#show'
-  get 'books/new' => 'books#new'
-  get 'books/edit' => 'books#edit'
+  resources :users
+  resources :books
+
+  get 'home/about' => 'homes#about'
+
 end
