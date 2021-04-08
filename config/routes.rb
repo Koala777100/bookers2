b8eccbfa35f6425895d6ac2ebee96cef877fbbac
Koala777100 <s_n_s_h@outlook.jp>
2021-04-08 12:30:49 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
 
   get 'home/about' => 'homes#about'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_064521) do
+ActiveRecord::Schema.define(version: 2021_04_08_120656) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id"
     t.text "title"
     t.text "body"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "profile_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
